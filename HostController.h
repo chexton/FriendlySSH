1471f5c44bf4b98e2eb4f45bdd3e80395ae2fd72
Chris Hexton <chrishexton@Chris-Hextons-iMac.local>
2011-04-12 15:09:32 +1000

    A small commit, after JB had a look.

---
+++ HostController.h
@@ -0,0 +1,17 @@
+//
+//  HostController.h
+//  FriendlySsh
+//
+//  Created by Chris Hexton on 12/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface HostController : NSWindowController {
+@private
+    
+}
+
+@end
---
+++ HostController.m
@@ -0,0 +1,36 @@
+//
+//  HostController.m
+//  FriendlySsh
+//
+//  Created by Chris Hexton on 12/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HostController.h"
+
+
+@implementation HostController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
